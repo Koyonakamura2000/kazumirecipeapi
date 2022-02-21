71d6989485689bff0eecfbe54032a86d24e547b9
Koyo Nakamura <koyonakamura123@gmail.com>
2022-02-20 17:34:26 -0800

    finished v1 of kazumi recipe api; currently set to all access

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, firestore, initialize_app
+
+
+
+cred = credentials.Certificate("kazumirecipekey.json")
+firebase_admin.initialize_app(cred)
+
+app = Flask(__name__)
+app.route('/addRecipe', methods='POST')
+
+db = firestore.client()
+
+recipe_ref = db.collection('recipes')
+
+
+@app.route('/', methods=['GET'])
+def recipes():
+    recipes = []
+    recipes_data = list(recipe_ref.get())
+    for recipe in recipes_data:
+        recipes.append(recipe.to_dict())
+
+    return jsonify(data=recipes)
+
+
+if __name__ == '__main__':
+    app.run()
